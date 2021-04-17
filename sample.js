f5cb23402272bbc3a67e58244ce3ba186c083f1b
hari500apps <56108078+hari500apps@users.noreply.github.com>
2021-04-17 14:08:25 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,43 @@
+// const prompt = require('prompt');
+
+// //prompt.start();
+
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// rl.question('What is your name ? ', function (name) {
+//   rl.question('Where do you live ? ', function (country) {
+//     console.log(`${name}, is a citizen of ${country}`);
+//     rl.close();
+//   });
+// });
+
+// rl.on('close', function () {
+//   console.log('\nBYE BYE !!!');
+//   process.exit(0);
+// });
+
+// // prompt.get(['username', 'email'], function (err, result) {
+// //   if (err) {
+// //     return onErr(err);
+// //   }
+// //   console.log('Command-line input received:');
+// //   console.log('  Username: ' + result.username);
+// //   console.log('  Email: ' + result.email);
+// // });
+
+// function onErr(err) {
+//   console.log(err);
+//   return 1;
+// }
+
+var counter = 0;
+while (true) {
+  for (i = 0; i < 5; i++) {
+    debugger; //enter this through Chrome Dev then press CTRL s to save
+    counter++;
+  }
+}
